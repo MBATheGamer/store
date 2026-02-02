ef8e0d9145349632400aa621a43b348383ef2ae6
MBATheGamer <mbathegamer@gmail.com>
2026-02-02 18:37:09 +0100

    Inject dependency via setter

--- src/main/java/com/mbathegamer/store/StoreApplication.java
+++ src/main/java/com/mbathegamer/store/StoreApplication.java
@@ -3,14 +3,18 @@ package com.mbathegamer.store;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mbathegamer.store.services.OrderService;
-import com.mbathegamer.store.services.StripePaymentService;
+import com.mbathegamer.store.services.PayPalPaymentService;
 
 @SpringBootApplication
 public class StoreApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(StoreApplication.class, args);
 
-		var orderService = new OrderService(new StripePaymentService());
+		var orderService = new OrderService(null);
+
+		orderService.placeOrder();
+
+		orderService.setPaymentService(new PayPalPaymentService());
 
 		orderService.placeOrder();
 	}
--- src/main/java/com/mbathegamer/store/services/OrderService.java
+++ src/main/java/com/mbathegamer/store/services/OrderService.java
@@ -7,7 +7,16 @@ public class OrderService {
     this.paymentService = service;
   }
 
+  public void setPaymentService(PaymentService paymentService) {
+    this.paymentService = paymentService;
+  }
+
   public void placeOrder() {
+    if (paymentService == null) {
+      System.out.println("You must set a payment service before using this method");
+      return;
+    }
+
     paymentService.processPayment(10);
   }
 }
