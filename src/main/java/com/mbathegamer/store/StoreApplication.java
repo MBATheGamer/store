2465d08a30850d363657b3fd76c56ff14c938cba
MBATheGamer <mbathegamer@gmail.com>
2026-02-02 18:54:26 +0100

    Configure beans using annotations

--- src/main/java/com/mbathegamer/store/StoreApplication.java
+++ src/main/java/com/mbathegamer/store/StoreApplication.java
@@ -1,21 +1,18 @@
 package com.mbathegamer.store;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import com.mbathegamer.store.services.OrderService;
-import com.mbathegamer.store.services.PayPalPaymentService;
 
 @SpringBootApplication
 public class StoreApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(StoreApplication.class, args);
 
-		var orderService = new OrderService(null);
-
-		orderService.placeOrder();
-
-		orderService.setPaymentService(new PayPalPaymentService());
-
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var orderService = context.getBean(OrderService.class);
 		orderService.placeOrder();
 	}
 }
--- src/main/java/com/mbathegamer/store/services/OrderService.java
+++ src/main/java/com/mbathegamer/store/services/OrderService.java
@@ -1,8 +1,16 @@
 package com.mbathegamer.store.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
   private PaymentService paymentService;
 
+  public OrderService() {
+  }
+
+  @Autowired
   public OrderService(PaymentService service) {
     this.paymentService = service;
   }
