6dd056f5229f9a70746fcfef63d64f816815af78
MBATheGamer <mbathegamer@gmail.com>
2026-02-02 18:57:14 +0100

    Controlling bean selection

--- src/main/java/com/mbathegamer/store/services/OrderService.java
+++ src/main/java/com/mbathegamer/store/services/OrderService.java
@@ -1,6 +1,7 @@
 package com.mbathegamer.store.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,7 @@ public class OrderService {
   }
 
   @Autowired
-  public OrderService(PaymentService service) {
+  public OrderService(@Qualifier("paypal") PaymentService service) {
     this.paymentService = service;
   }
 
--- src/main/java/com/mbathegamer/store/services/StripePaymentService.java
+++ src/main/java/com/mbathegamer/store/services/StripePaymentService.java
@@ -1,5 +1,10 @@
 package com.mbathegamer.store.services;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service("stripe")
 public class StripePaymentService implements PaymentService {
   @Override
   public void processPayment(double amount) {
