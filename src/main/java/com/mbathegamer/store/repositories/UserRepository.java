f7a38320fcbe6177d022d66914ecfb39c8ed42e6
MBATheGamer <mbathegamer@gmail.com>
2026-02-05 00:37:07 +0100

    Clean repository

--- src/main/java/com/mbathegamer/store/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mbathegamer.store.repositories;
-
-import com.mbathegamer.store.models.User;
-
-public interface UserRepository {
-  void save(User user);
-
-  User findUserByEmail(String email);
-}
--- src/main/java/com/mbathegamer/store/services/PaymentService.java
+++
@@ -1,5 +0,0 @@
-package com.mbathegamer.store.services;
-
-public interface PaymentService {
-  void processPayment(double amount);
-}
