872ae87d51ff5fcab0cd47f6a394bc10f3e52e9e
MBATheGamer <mbathegamer@gmail.com>
2026-02-05 02:44:20 +0100

    Add `description` field to `Product` entity

--- src/main/java/com/mbathegamer/store/entities/Product.java
+++ src/main/java/com/mbathegamer/store/entities/Product.java
@@ -34,6 +34,9 @@ public class Product {
   @Column(name = "name")
   private String name;
 
+  @Column(name = "description")
+  private String description;
+
   @Column(name = "price")
   private BigDecimal price;
 
