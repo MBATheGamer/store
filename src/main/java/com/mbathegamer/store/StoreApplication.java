41a9a0019fa09ebc77967356ab90d511cbf1b03b
MBATheGamer <mbathegamer@gmail.com>
2026-02-13 03:44:52 +0100

    Complete part 1

--- src/main/java/com/mbathegamer/store/StoreApplication.java
+++ src/main/java/com/mbathegamer/store/StoreApplication.java
@@ -4,9 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.mbathegamer.store.services.UserService;
+
 @SpringBootApplication
 public class StoreApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var service = context.getBean(UserService.class);
+		// service.showEntityStates();
+		// service.showRelatedEntities();
+		// service.fetchAddress();
+		// service.persistRelated();
+		// service.deleteRelated();
+		// service.manageProducts();
+		// service.updateProductPrice();
+		// service.fetchProducts();
+		// service.fetchProductByCriteria();
+		// service.fetchProductsBySpecifications("prod", null, null);
+		// service.fetchUser();
+		// service.printLoyalProfiles();
+		service.fetchSortedProducts();
+		service.fetchPaginatedProduct(0, 20);
 	}
 }
